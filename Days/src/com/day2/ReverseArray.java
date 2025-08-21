90f5c9c2a208e1ab28252bd8e54d18086fa81894
Akash Bhusare <bhusareakash88@gmail.com>
2025-08-22 01:10:05 +0530

    Day 2 added

---
+++ Days/src/com/day2/ReverseArray.java
@@ -0,0 +1,26 @@
+package com.day2;
+
+public class ReverseArray {
+
+	public static void main(String[] args) {
+		
+		int[] arr = {1,2,3,4,5,6};
+		
+		int start = 0;
+		int end = arr.length-1;
+		
+		while(start < end) {
+			int temp = arr[start];
+			arr[start] = arr[end];
+			arr[end] = temp;
+			
+			start++;
+			end--;
+		}
+		
+		for(int ar : arr) {
+			System.out.print(ar);
+		}
+		
+	}
+}
